d8fe45b2a6c4f59bc56d597b9e87948bf879fd00
Abraham May <AbrahamMay1015@gmail.com>
2024-05-08 18:46:51 -0400

    Was able to add my own voice

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("play-audio-tenacious").addEventListener("click", function() {
+    var audio = new Audio("C:\\Users\\Abraham\\Desktop\\Tenacious.mp3");
+    audio.play();
+});
