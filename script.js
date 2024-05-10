0aebfc9a6335ed0bcad89ddd2dcbba00b9cef5c6
Abraham May <AbrahamMay1015@gmail.com>
2024-05-09 22:59:11 -0400

    voice

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("play-audio-tenacious").addEventListener("click", function() {
-    var audio = new Audio("C:\\Users\\Abraham\\Desktop\\Tenacious.mp3");
-    audio.play();
-});
